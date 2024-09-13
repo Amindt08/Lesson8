7b947bc042b08b51e36a619909db2815262e9b9a
AMINDTMSTR\User <amindtmstr>
2024-09-13 20:41:38 +0700

    Lesson 8 - Unscramble

---
+++ app/src/main/java/com/example/unscrambel/data/WordsData.kt
@@ -0,0 +1,22 @@
+package com.example.unscrambel.data
+
+const val MAX_NO_OF_WORDS = 10
+const val SCORE_INCREASE = 20
+
+// Set with all the words for the Game
+val allWords: Set<String> =
+    setOf(
+        "TELO",
+        "LONTHONG",
+        "IWAK",
+        "SATE",
+        "RAWON",
+        "PECEL",
+        "SEBLAK",
+        "BAKSO ACI",
+        "WONTON",
+        "BAKSO",
+        "ES TELER",
+        "PENTOL",
+        "AYAM",
+    )
